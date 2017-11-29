f9cf191aabe3407b587c6431e6fdf03313b420a3
Gonza420 <gonzalomcardoso@gmail.com>
2017-11-28 21:17:08 -0300

    final

--- src/main/java/com/ungs/espe2017/model/service/ServiciosUsuario.java
+++ src/main/java/com/ungs/espe2017/model/service/ServiciosUsuario.java
@@ -1,7 +1,7 @@
 package com.ungs.espe2017.model.service;
 
-import java.util.ArrayList;
 import java.util.List;
+
 import com.ungs.espe2017.model.dao.IUsuarioDAO;
 import com.ungs.espe2017.model.domain.Usuario;
 
@@ -14,45 +14,54 @@ public class ServiciosUsuario {
     	dao.crear(entry);
     }
     
-    private List<Usuario> findAll() {
-		List<Usuario> myList = new ArrayList<Usuario>();
-		myList = dao.todos();
-		return myList;
-	}
-    
-    
-    public Usuario buscarUsuario (String e) {
+    public Usuario obtenerUsuario (String email) {
     	List<Usuario> lista = dao.todos();
     	for (Usuario user : lista) {
-    		if(user.getEmail().equals(e))
+    		if(user.getEmail().equals(email))
     		return user; 
     	
     	}
-    	return null;
+    	return new Usuario();
 	}
 	
-
-	public boolean seAceptaLogin(String email, String password){
-		//if(email.equals(buscarUsuario(email).getEmail()))
-			return true;
-		//else
-		//return false;
+    public boolean seAceptaLogin (String email, String password) {
+    	List<Usuario> lista = dao.todos();
+    	for (Usuario user : lista) {
+    		if(user.getEmail().equals(email) && user.getPassword().equals(password))
+    		return true; 
+    	
+    	}
+    	return false;
 	}
 	
 	
-	public boolean seAceptaRegistro(Usuario user){
-		//if (buscarUsuario(user.getEmail()).equals(null))
-		//{
-			save(user);
+	
+	public boolean seAceptaRegistro(Usuario usuario){
+    	List<Usuario> lista = dao.todos();
+    	for (Usuario user : lista) {
+    		if(user.getEmail().equals(usuario.getEmail()))
+    		return false; 
+    	
+    	}
+			save(usuario);
 			return true;
-		//}
-		//else
-		//	return false;
 			
 	}
 	
 	
-	
+    public static void main(String[] args) {
+    	IUsuarioDAO dao = new IUsuarioDAO();
+    	List<Usuario> lista = dao.todos();
+    	for (Usuario user : lista) {
+    		if(user.getEmail().equals(null)) 
+    		System.out.println("Existe:"); 
+
+    	
+    	}
+		
+								
+		}
+
 	
 
 	
--- src/main/java/com/ungs/espe2017/view/controller/Perfil.java
+++ src/main/java/com/ungs/espe2017/view/controller/Perfil.java
@@ -1,6 +1,9 @@
 package com.ungs.espe2017.view.controller;
 
+import com.ungs.espe2017.model.domain.Usuario;
+import com.ungs.espe2017.model.service.ServiciosUsuario;
 import com.vaadin.navigator.View;
+import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
@@ -9,6 +12,8 @@ public class Perfil extends VerticalLayout implements View  {
 		public static final String NAME = "Perfil";
 		
 		private static final long serialVersionUID = 1L;
+		
+		private ServiciosUsuario service = new ServiciosUsuario();
 		
 	    
 		
@@ -20,7 +25,9 @@ public class Perfil extends VerticalLayout implements View  {
 	public Perfil() {
 		// TODO Auto-generated constructor stub
 		super();
-		addComponent(new Label("ESETE ES EL PERFIL GATO"));
+		addComponent(new Label("ESTE ES EL PERFIL"));
+		Usuario usuario=service.obtenerUsuario((String) VaadinSession.getCurrent().getAttribute("username"));
+		addComponent(new Label(usuario.toString()));
 	}
 
 }
